7b56da72b93351b324733550a28dc49e338aaef0
Minjae <colton950901@gmail.com>
2022-06-08 23:38:51 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        "source": "/(.*)",
+        "headers" : [
+          {
+            "key" : "ETag"
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
